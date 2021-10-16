6c70e7f7d0490cda8f2a686692799558667535b1
Joao_Marcos <joaobastos716@gmail.com>
2021-10-16 01:00:18 +0000

    Primeira versão do projeto 10;15;2021

--- app/models.py
+++ app/models.py
@@ -18,11 +18,43 @@ def get_user(user_id):
     return User.query.filter_by(id=user_id).first()
 
 class User(db.Model, UserMixin): #usuarios
-
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     name = db.Column(db.String(70), nullable=False)
     email = db.Column(db.String(70), nullable=False, unique=True)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(2048), nullable=False)
+
+
+#class perfil(db.Model, UserMixin):
+   # user_id = Column(db.Integer, ForeignKey('User.id'), nullable=False)
+    #name = Column(db.String, ForeignKey('User.name'), nullable=False)
+   # email = Column(db.String, ForeignKey('User.email'), nullable=False)
+    #Caso o usuário queira redefinir a senha, redirecione para a tela de login e clique em "Esqueci minha senha"
+
+
+
+
+
+
+
+#class Receitas(db.Model):
+ #   id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+  #  titulo = db.Column(db.String(50), nullable=False)
+   # desc = db.Column(db.String(100), nullable=False)
+    #tempo_preparo = db.Column(db.DateTime(), nullable=False)
+    #rendimento = db.Column(db.String(50), nullable=False)
+    #imagem = db.Column(db.LargeBinary, nullable =False)
+    #userid = db.Column('user_id', Integer, ForeignKey("User.user_id"), nullable=False)
+
+
+
+
+
+
+
+#class passo(db.Model):
+  #  id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+
+
 
     def __init__(self, name, email, password):
         self.name = name
@@ -31,3 +63,17 @@ class User(db.Model, UserMixin): #usuarios
 
     def verify_password(self, pwd):
         return check_password_hash(self.password, pwd)
+
+    def __str__(self):
+        return f'Usuário {self.name} tem o e-mail {self.email}'
+
+    # def __init__(self, titulo, desc, tempo_preparo, rendimento, imagem, userid):
+       #  self.titulo = titulo
+        # self.desc = desc
+       #  self.tempo_preparo = tempo_preparo
+       #  self.rendimento = rendimento
+       #  self.imagem = imagem
+        # self.userid = userid
+
+
+
