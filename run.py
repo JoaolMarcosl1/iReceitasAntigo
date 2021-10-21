dcff409ee3c360ec1a1a988092fedda610426a67
Joao_Marcos <joaobastos716@gmail.com>
2021-10-21 00:45:46 +0000

    Inserindo mensagens flash, modificações com o jinja e pequenas alterações na homepage.

--- run.py
+++ run.py
@@ -1,16 +1,18 @@
 from flask import render_template, request, redirect, url_for, flash, session
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, current_user, login_manager
 from app import app, db
 from app.models import User
 from flask_mail import Mail, Message
-######################################
+
+
+
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL']= True
 app.config['MAIL_USE_TLS']= False
 app.config['MAIL_USERNAME'] = 'receitasprojetoint@gmail.com'
 app.config['MAIL_PASSWORD'] = 'receitas2021proj'
-######################################
+
 mail = Mail(app)
 
 
@@ -20,28 +22,15 @@ mail = Mail(app)
 #from  flask.ext.bcrypt  import  Bcrypt
 #app  =  Flask ( __name__ )
 #bcrypt  =  Bcrypt ( app )
-
-@app.route('/contato', methods=['GET', 'POST'])
-def contato():
-    if request.method == 'POST':
-        tittle = request.form.get('tittle')
-        message = request.form.get('message')
-
-        msg = Message(subject=f"Tittle: {tittle}", body=f"\nConteudo: {message}", sender="joaobastos716@gmail.com", recipients=["receitasprojetoint@gmail.com"])
-        mail.send(msg)
-        #msg = Message("Olá, Estou precisando da ajuda de vocês.", sender="joaobastos716@gmail.com", recipients=["receitasprojetoint@gmail.com"])
-        #msg.body = "Enviando uma duvida, testando"
-        #mail.send(msg)
-        return "Email enviado com sucesso"
-
-
-    return render_template("contato.html")
-
-
-
 @app.route('/home')
 @app.route('/')
+#@login_manager
 def home():
+    if not current_user.is_authenticated:
+        flash("\nVocê não esta logado.")
+    else:
+        flash(f"\n\nOlá {current_user.name}, seja bem vindo(a).")
+
     return render_template("home.html")
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -54,16 +43,36 @@ def register():
         jatem = User.query.filter_by(email=email).first()
 
         if jatem is not None:
-            flash('Já existe uma conta com esse e-mail :)')
+            flash('Já existe uma conta com esse e-mail. Insira outro e-mail')
             return redirect(url_for('register'))
 
         else:
             user = User(name, email, pwd)
             db.session.add(user) #inserir
             db.session.commit()  #atualiza
+            flash('Conta criada com sucesso!')
             return redirect(url_for('login'))
 
+
     return render_template('register.html')
+
+@app.route('/contato', methods=['GET', 'POST'])
+def contato():
+    if request.method == 'POST':
+        tittle = request.form.get('tittle')
+        message = request.form.get('message')
+
+        #message = message+f"\nReperquilson se garante mais que o {current_user.name}"
+
+        msg = Message(subject=f"Help {current_user.name}: {tittle}", body=f"\n{current_user.name}:{message}", sender="joaobastos716@gmail.com", recipients=["receitasprojetoint@gmail.com"])
+        mail.send(msg)
+        #msg = Message("Olá, Estou precisando da ajuda de vocês.", sender="joaobastos716@gmail.com", recipients=["receitasprojetoint@gmail.com"])
+        #msg.body = "Enviando uma duvida, testando"
+        #mail.send(msg)
+        return redirect(url_for('home'))
+
+
+    return render_template("contato.html")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -78,7 +87,7 @@ def login():
             return redirect(url_for('login'))
 
         login_user(user)
-        flash('You were successfully logged in')
+        flash('Você foi logado com sucesso :)\n')
         return redirect(url_for('home'))
 
     return render_template('login.html')
@@ -94,12 +103,25 @@ def edit(id):
 
         jatem = User.query.filter_by(email=user.email).first()
 
-        if jatem is not None:
+        if not jatem or not jatem.email == user.email:
             return "E-mail existe"
 
         else:
             db.session.commit()
-            return redirect(url_for('home'))
+            return "Deu certo, você mudou o e-mail"
+
+
+
+       # db.session.commit()
+       # return redirect(url_for('home'))
+
+        #jatem = User.query.filter_by(email=user.email).first()
+
+       # if jatem is not None:
+           # return "E-mail existe"
+
+      #  else:
+
 
     return render_template("edit.html", user=user)
 
@@ -139,7 +161,7 @@ def perfil():
 @app.route('/logout')
 def logout():
     logout_user()
-    flash('You were logged out')
+    flash('\nVocê foi desconectado.\n')
     return redirect(url_for('home'))
 
 
